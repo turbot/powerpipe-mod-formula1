fb39ea5db2d80a92eeb7a7122b13be2b087c70be
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-04 20:30:33 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -3,5 +3,5 @@ mod "formula1" {
   description   = "Zoom into the high-speed world of Formula 1 with the Formula 1 Dashboard, offering a detailed overview of races, drivers, constructors, and seasons, alongside in-depth performance analysis, driver statistics, race highlights, and insights into the global distribution of drivers and circuits, using SQLite with Powerpipe."
   color         = "#FF1801"
   documentation = file("./README.md")
-  categories    = ["sqlite"]
+  categories    = ["dashboard", "sqlite"]
 }
