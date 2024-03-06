dbfd0fb134d3578bad87a2d43429c4a387825a57
misraved <ved@turbot.com>
2024-03-06 20:54:28 +0530

    Add .github, .gitignore and .gitatributes files

--- docs/index.md
+++ docs/index.md
@@ -43,7 +43,7 @@ Start the dashboard server with the DB connection string:
 powerpipe server --database sqlite:Formula1.sqlite
 ```
 
-Browse and view your dashboards at **https://localhost:9033**.
+Browse and view your dashboards at **http://localhost:9033**.
 
 ## Open Source & Contributing
 
--- mod.pp
+++ mod.pp
@@ -2,7 +2,8 @@ mod "formula1" {
   title         = "Formula 1"
   description   = "Zoom into the high-speed world of Formula 1 with the Formula 1 Dashboard, offering a detailed overview of races, drivers, constructors, and seasons, alongside in-depth performance analysis, driver statistics, race highlights, and insights into the global distribution of drivers and circuits, using SQLite with Powerpipe."
   color         = "#FF1801"
-  documentation = file("./README.md")
+  documentation = file("./docs/index.md")
+  icon          = "/images/mods/turbot/formula1.svg"
   categories    = ["dashboard", "sqlite"]
 
   opengraph {
