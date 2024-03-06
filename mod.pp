c2c5d9bfb30b87dcc0f3e7f4ba1531a21f6657dc
misraved <ved@turbot.com>
2024-03-06 21:21:23 +0530

    Update social graphics and icon

--- mod.pp
+++ mod.pp
@@ -3,12 +3,12 @@ mod "formula1" {
   description   = "Zoom into the high-speed world of Formula 1 with the Formula 1 Dashboard, offering a detailed overview of races, drivers, constructors, and seasons, alongside in-depth performance analysis, driver statistics, race highlights, and insights into the global distribution of drivers and circuits, using SQLite with Powerpipe."
   color         = "#FF1801"
   documentation = file("./docs/index.md")
-  icon          = "/images/mods/turbot/formula1.svg"
+  icon          = "/images/mods/turbot/formula1-dashboard.svg"
   categories    = ["dashboard", "sqlite"]
 
   opengraph {
     title       = "Powerpipe Mod for Formula 1"
     description = "Zoom into the high-speed world of Formula 1 with the Formula 1 Dashboard, offering a detailed overview of races, drivers, constructors, and seasons, alongside in-depth performance analysis, driver statistics, race highlights, and insights into the global distribution of drivers and circuits, using SQLite with Powerpipe."
-    image       = "/images/mods/turbot/formula1-social-graphic.png"
+    image       = "/images/mods/turbot/formula1-dashboard-social-graphic.png"
   }
 }
