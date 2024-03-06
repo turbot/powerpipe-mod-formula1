2c0f5c527552834dd8a2d01cbcbbb1a9aa12e2da
sourav chakraborty <sourav@turbot.com>
2024-03-06 16:58:37 +0530

    update

--- mod.pp
+++ mod.pp
@@ -4,10 +4,10 @@ mod "formula1" {
   color         = "#FF1801"
   documentation = file("./README.md")
   categories    = ["dashboard", "sqlite"]
-}
 
-opengraph {
-  title       = "Powerpipe Mod for Formula 1"
-  description = "Zoom into the high-speed world of Formula 1 with the Formula 1 Dashboard, offering a detailed overview of races, drivers, constructors, and seasons, alongside in-depth performance analysis, driver statistics, race highlights, and insights into the global distribution of drivers and circuits, using SQLite with Powerpipe."
-  image       = "/images/mods/turbot/formula1-social-graphic.png"
+  opengraph {
+    title       = "Powerpipe Mod for Formula 1"
+    description = "Zoom into the high-speed world of Formula 1 with the Formula 1 Dashboard, offering a detailed overview of races, drivers, constructors, and seasons, alongside in-depth performance analysis, driver statistics, race highlights, and insights into the global distribution of drivers and circuits, using SQLite with Powerpipe."
+    image       = "/images/mods/turbot/formula1-social-graphic.png"
+  }
 }
